78c3b8fb213d0ecf63c2a19849b1ae4925435b10
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-02-03 20:11:46 +0100

    fix: make the coding style python code style

--- tests/test_models/test_election.py
+++ tests/test_models/test_election.py
@@ -3,18 +3,24 @@ import unittest
 from datetime import datetime, timedelta
 from models.election import Election
 
+
 class TestElection(unittest.TestCase):
     def setUp(self):
         """ Define a setUp method """
-        self.election = Election("Test Election", 
-                                 (datetime.now() + timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S'), 
-                                 (datetime.now() + timedelta(days=2)).strftime('%Y-%m-%d %H:%M:%S'), 
+        start_date = (datetime.now()
+                      + timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S')
+        end_date = (datetime.now()
+                    + timedelta(days=2)).strftime('%Y-%m-%d %H:%M:%S')
+        self.election = Election("Test Election",
+                                 start_date,
+                                 end_date,
                                  ["Candidate 1", "Candidate 2"])
 
     def test_init(self):
         """ Test that new instance is instantiated as expected """
         self.assertEqual(self.election.title, "Test Election")
-        self.assertEqual(self.election.candidates, ["Candidate 1", "Candidate 2"])
+        self.assertEqual(self.election.candidates,
+                         ["Candidate 1", "Candidate 2"])
         self.assertEqual(self.election.voters, [])
         self.assertEqual(self.election.ballots, [])
         self.assertEqual(self.election.status, "Upcoming")
@@ -30,83 +36,106 @@ class TestElection(unittest.TestCase):
             E1 = Election()
 
         with self.assertRaises(TypeError) as cm:
-            E2 = Election(12, 
-                                (datetime.now() + timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S'), 
-                                (datetime.now() + timedelta(days=2)).strftime('%Y-%m-%d %H:%M:%S'), 
-                                ["Candidate 1", "Candidate 2"])
+            Election(12,
+                     (datetime.now()
+                      + timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S'),
+                     (datetime.now()
+                      + timedelta(days=2)).strftime('%Y-%m-%d %H:%M:%S'),
+                     ["Candidate 1", "Candidate 2"])
 
         self.assertEqual(str(cm.exception), "Title must be a string")
 
     def test_init_title_empty(self):
         """ Test that title parameter cannot be empty """
         with self.assertRaises(ValueError) as cm:
-            Election("", 
-                     (datetime.now() + timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S'), 
-                     (datetime.now() + timedelta(days=2)).strftime('%Y-%m-%d %H:%M:%S'), 
+            Election("",
+                     (datetime.now()
+                      + timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S'),
+                     (datetime.now()
+                      + timedelta(days=2)).strftime('%Y-%m-%d %H:%M:%S'),
                      ["Candidate 1", "Candidate 2"])
         self.assertEqual(str(cm.exception), "Title is required")
 
     def test_init_start_date_validation(self):
         """ Test that start date is instantiated as expected """
         with self.assertRaises(ValueError) as cm:
-            E3 = Election("Test", "test_time", 
-                          (datetime.now() + timedelta(days=2)).strftime('%Y-%m-%d %H:%M:%S'), 
-                          ["Candidate 1", "Candidate 2"])
-        self.assertEqual(str(cm.exception), "Invalid date format. Use 'YYYY-MM-DD HH:MM:SS'")
+            Election("Test", "test_time",
+                     (datetime.now()
+                      + timedelta(days=2)).strftime('%Y-%m-%d %H:%M:%S'),
+                     ["Candidate 1", "Candidate 2"])
+        self.assertEqual(str(cm.exception),
+                         "Invalid date format. Use 'YYYY-MM-DD HH:MM:SS'")
 
     def test_init_start_date_empty(self):
         """ Test that start_date parameter cannot be empty """
         with self.assertRaises(ValueError) as cm:
-            Election("Test Election", 
-                    "", 
-                    (datetime.now() + timedelta(days=2)).strftime('%Y-%m-%d %H:%M:%S'), 
-                    ["Candidate 1", "Candidate 2"])
-        self.assertEqual(str(cm.exception), "Election starting date is required in the format 'YYYY-MM-DD HH:MM:SS'")
+            Election("Test Election",
+                     "",
+                     (datetime.now()
+                      + timedelta(days=2)).strftime('%Y-%m-%d %H:%M:%S'),
+                     ["Candidate 1", "Candidate 2"])
+        self.assertEqual(str(cm.exception),
+                         ("Election starting date is required in the "
+                          "format 'YYYY-MM-DD HH:MM:SS'"))
 
     def test_init_end_date_empty(self):
         """ Test that end_date parameter cannot be empty """
         with self.assertRaises(ValueError) as cm:
-            Election("Test Election", 
-                    (datetime.now() + timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S'), 
-                    "", 
-                    ["Candidate 1", "Candidate 2"])
-        self.assertEqual(str(cm.exception), "Election ending date is required in the format 'YYYY-MM-DD HH:MM:SS'")
+            Election("Test Election",
+                     (datetime.now()
+                      + timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S'),
+                     "",
+                     ["Candidate 1", "Candidate 2"])
+        self.assertEqual(str(cm.exception),
+                         ("Election ending date is required in the "
+                          "format 'YYYY-MM-DD HH:MM:SS'"))
 
     def test_init_candidates_list_empty(self):
         """ Test that candidates_list parameter cannot be empty """
         with self.assertRaises(ValueError) as cm:
-            Election("Test Election", 
-                    (datetime.now() + timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S'), 
-                    (datetime.now() + timedelta(days=2)).strftime('%Y-%m-%d %H:%M:%S'), 
-                    [])
+            Election("Test Election",
+                     (datetime.now()
+                      + timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S'),
+                     (datetime.now()
+                      + timedelta(days=2)).strftime('%Y-%m-%d %H:%M:%S'),
+                     [])
         self.assertEqual(str(cm.exception), "Candidates' list is required")
 
     def test_init_start_date_after_end_date(self):
         """ Test that start_date cannot be after end_date """
         with self.assertRaises(ValueError) as cm:
-            Election("Test Election", 
-                    (datetime.now() + timedelta(days=2)).strftime('%Y-%m-%d %H:%M:%S'), 
-                    (datetime.now() + timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S'), 
-                    ["Candidate 1", "Candidate 2"])
-        self.assertEqual(str(cm.exception), "Election starting date must be before the ending date")
-           
+            Election("Test Election",
+                     (datetime.now()
+                      + timedelta(days=2)).strftime('%Y-%m-%d %H:%M:%S'),
+                     (datetime.now()
+                      + timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S'),
+                     ["Candidate 1", "Candidate 2"])
+        self.assertEqual(str(cm.exception),
+                         ("Election starting date must be before "
+                          "the ending date"))
+
     def test_init_candidates_not_list(self):
         """ Test that candidates_list parameter must be a list """
         with self.assertRaises(TypeError) as cm:
-            Election("Test Election", 
-                     (datetime.now() + timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S'), 
-                     (datetime.now() + timedelta(days=2)).strftime('%Y-%m-%d %H:%M:%S'), 
+            Election("Test Election",
+                     (datetime.now()
+                      + timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S'),
+                     (datetime.now()
+                      + timedelta(days=2)).strftime('%Y-%m-%d %H:%M:%S'),
                      "Candidate 1, Candidate 2")
         self.assertEqual(str(cm.exception), "Candidates' list must be a list")
 
     def test_init_candidates_not_strings(self):
         """ Test that candidates_list parameter must contain strings only """
         with self.assertRaises(TypeError) as cm:
-            Election("Test Election", 
-                    (datetime.now() + timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S'), 
-                    (datetime.now() + timedelta(days=2)).strftime('%Y-%m-%d %H:%M:%S'), 
-                    [1, 2, 3])
-        self.assertEqual(str(cm.exception), "Candidates' list must contain strings only")
+            Election("Test Election",
+                     (datetime.now()
+                      + timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S'),
+                     (datetime.now()
+                      + timedelta(days=2)).strftime('%Y-%m-%d %H:%M:%S'),
+                     [1, 2, 3])
+        self.assertEqual(str(cm.exception),
+                         "Candidates' list must contain strings only")
 
     def test_add_candidate(self):
         """ Test that a """
@@ -115,8 +144,11 @@ class TestElection(unittest.TestCase):
 
     def test_add_voter(self):
         self.election.add_voter("John", "Doe", "john.doe@example.com")
-        self.assertIn({"first_name": "John", "last_name": "Doe", "email": "john.doe@example.com"}, self.election.voters)
+        self.assertIn({"first_name": "John", "last_name": "Doe",
+                       "email": "john.doe@example.com"}, self.election.voters)
 
     def test_make_ballot(self):
         self.election.make_ballot("Ballot 1", ["Candidate 1", "Candidate 3"])
-        self.assertIn({"name": "Ballot 1", "candidates": ["Candidate 1", "Candidate 3"]}, self.election.ballots)
+        self.assertIn({"name": "Ballot 1",
+                       "candidates": ["Candidate 1", "Candidate 3"]},
+                      self.election.ballots)
