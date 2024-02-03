ebae21400190a9395d87c7cc313c720181d9ac70
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-02-03 19:08:10 +0100

    Add to_dict and get_status methods

--- models/election.py
+++ models/election.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """ This module defines the Election class
 """
+from datetime import datetime
+from shortuuid import ShortUUID
+from uuid import uuid4
 
 
 class Election():
     """ The Election class
     """
-    from datetime import datetime
-    from shortuuid import ShortUUID
-    from uuid import uuid4
-
     def __init__(self, title, start_date, end_date, candidates_list):
         """ Initializes Election class.
 
@@ -26,35 +25,41 @@ class Election():
         if not title:
             raise ValueError("Title is required")
         if not isinstance(title, str):
-            raise ValueError("Title must be a string")
+            raise TypeError("Title must be a string")
 
         # Validate received dates as strings
         # in the format 'YYYY-MM-DD HH:MM:SS'
         if not start_date:
-            raise ValueError("Election starting date is required in
-                             the format 'YYYY-MM-DD HH:MM:SS'")
+            msg = "Election starting date is required in the " \
+                "format 'YYYY-MM-DD HH:MM:SS'"
+            raise ValueError(msg)
+        if not isinstance(start_date, str):
+            raise TypeError("Election starting date must be a string")
         if not end_date:
-            raise ValueError("Election ending date is required in
-                             the format 'YYYY-MM-DD HH:MM:SS'")
+            msg = "Election ending date is required in the " \
+                "format 'YYYY-MM-DD HH:MM:SS'"
+            raise ValueError(msg)
+        if not isinstance(end_date, str):
+            raise TypeError("Election ending date must be a string")
         try:
             start_date = datetime.strptime(start_date, '%Y-%m-%d %H:%M:%S')
             end_date = datetime.strptime(end_date, '%Y-%m-%d %H:%M:%S')
         except ValueError:
             raise ValueError("Invalid date format. Use 'YYYY-MM-DD HH:MM:SS'")
         if start_date > end_date:
-            raise ValueError("Election starting date must be
-                             before the ending date")
+            msg = "Election starting date must be before the ending date"
+            raise ValueError(msg)
 
         # Validate the candidates_list parameter
         if not candidates_list:
             raise ValueError("Candidates' list is required")
         if not isinstance(candidates_list, list):
-            raise ValueError("Candidates' list must be a list")
+            raise TypeError("Candidates' list must be a list")
         if not all(isinstance(candidate, str)
                    for candidate in candidates_list):
-            raise ValueError("Candidates' list must contain strings only")
+            raise TypeError("Candidates' list must contain strings only")
 
-        self.public_key = short_uuid()
+        self.public_key = self.short_uuid()
         self.private_key = str(uuid4())
         self.title = title
         self.start_date = start_date
@@ -82,6 +87,8 @@ class Election():
         Returns:
             None
         """
+        if not isinstance(candidate, str):
+            raise ValueError("Election starting date must be a string")
         self.candidates.append(candidate)
 
     def add_voter(self, first_name, last_name, email):
@@ -126,10 +133,7 @@ class Election():
     def get_voters(self):
         """ Get the list of voters
         """
-        voters = []
-        voters_count = len(self.voters)
-
-        return self.voters
+        return {"voters_count": len(self.voters), "voters": self.voters}
 
     def start_election(self):
         """ Start the election
@@ -139,9 +143,31 @@ class Election():
     def end_election(self):
         """ End the election
         """
-        self.status = "Ended"
+        self.status = "Completed"
 
     def get_results(self):
         """ Get the election results
         """
         return self.results
+
+    def get_election_status(self):
+        """ Determine election status
+        """
+        current_time = datetime.now()
+
+        if current_time < self.start_date:
+            self.status = "Upcoming"
+        elif self.start_date <= current_time <= self.end_date:
+            self.status = "Ongoing"
+        else:
+            self.status = "Completed"
+
+        return self.status
+
+    def to_dict(self):
+        """ Returns dictionary representation of an election instance
+        """
+        # Get a copy of instance dict
+        dict_copy = self.__dict__.copy()
+        dict_copy["__class__"] = f"{self.__class__.__name__}"
+        return dict_copy
