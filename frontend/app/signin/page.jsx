a2a510304babd64db3cc9e318bead9c96074ed44
Mahama Hamida <mahamahamida@gmail.com>
2024-02-04 02:31:54 +0000

    Add SignInPage component and use Next.js Image component for logo

---
+++ frontend/app/signin/page.jsx
@@ -0,0 +1,22 @@
+export default function SignInPage() {
+    return(
+        <form>
+            <div className="card " style={{borderRadius: "20px", overFlow: "hidden"}}>
+                <h1 className="card-title mb-4"><b>Sign in</b></h1>
+                <p className="card-subtitle">Please enter your details to sign in</p>
+                <div className="form-floating mb-3">
+                    <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                    <label htmlFor="floatingInput">Email address</label>
+                </div>
+
+                <div className="form-floating">
+                    <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+                    <label htmlFor="floatingPassword">Password</label>
+                </div>
+                <div className="d-grid gap-2">
+                    <button href="#" className="btn btn-primary">Submit</button>
+                </div>
+            </div>
+        </form>
+    )
+}
--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import Logo from '@/public/logo.png'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg border-bottom border-body bg-light mt-2 mb-2">
             <a className="navbar-brand navbar-nav" href="#">
-                <img id="logo" src={Logo} alt="PollMaster" />
+                <Image id="logo" src={Logo} alt="PollMaster" />
             </a>
         </nav>
     )
