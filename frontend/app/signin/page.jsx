fd83073e3c8bb28709a24ea9abd8dda39628a778
Mahama Hamida <mahamahamida@gmail.com>
2024-02-05 02:16:20 +0000

    Refactor sign-in page to use custom Button component

--- frontend/app/signin/page.jsx
+++ frontend/app/signin/page.jsx
@@ -1,8 +1,9 @@
+import Button from "../components/Button"
 export default function SignInPage() {
     return(
         <form>
             <div className="card " style={{borderRadius: "20px", overFlow: "hidden"}}>
-                <h1 className="card-title mb-4"><b>Sign in</b></h1>
+                <h1 className="card-title mb-4">Sign in</h1>
                 <p className="card-subtitle">Please enter your details to sign in</p>
                 <div className="form-floating mb-3">
                     <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
@@ -14,7 +15,8 @@ export default function SignInPage() {
                     <label htmlFor="floatingPassword">Password</label>
                 </div>
                 <div className="d-grid gap-2">
-                    <button href="#" className="btn btn-primary">Submit</button>
+                    {/* <button href="#" className="btn btn-primary">Submit</button> */}
+                    <Button text={"Sign In"} />
                 </div>
             </div>
         </form>
