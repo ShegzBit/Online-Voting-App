980b299986a1628139e71b66bb60b33df19a4a72
Mahama Hamida <mahamahamida@gmail.com>
2024-02-07 11:23:05 +0000

    Update Fab component to use NewProjectModal

--- frontend/app/components/Fab.jsx
+++ frontend/app/components/Fab.jsx
@@ -1,6 +1,6 @@
 import { FaPlus } from 'react-icons/fa6'
 import { useState } from 'react';
-import Modal from './Modal';
+import NewProjectModal from './modals/NewProject';
 
 export default function Fab() {
     const [isOpen, setIsOpen] = useState(false)
@@ -9,11 +9,11 @@ export default function Fab() {
         setIsOpen(!isOpen)
     }
     if (isOpen) {
-        return <Modal />
+        return <NewProjectModal />
     }
     return (
         <div className="position-fixed bottom-0 end-0 me-2 mb-2 shadow-sm">
-            <button type="button" className="bg-success btn-gradient text-light rounded-circle border-0" aria-label="Add new project" style={{ width: "56px", height: "56px" }} data-bs-toggle="modal" data-bs-target="#exampleModal"><FaPlus /></button>
+            <button type="button" className="bg-success btn-gradient text-light rounded-circle border-0" aria-label="Add new project" style={{ width: "56px", height: "56px" }} data-bs-toggle="modal" data-bs-target="#newProject"><FaPlus /></button>
         </div>
     )
 }
