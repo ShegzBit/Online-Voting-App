9311a72aa0bb3384862de8b3583eab69c26d3d11
Mahama Hamida <mahamahamida@gmail.com>
2024-02-08 00:49:36 +0000

    Refactor Fab component to handle open and close states

--- frontend/app/components/Fab.jsx
+++ frontend/app/components/Fab.jsx
@@ -5,15 +5,20 @@ import NewProjectModal from './modals/NewProject';
 export default function Fab() {
     const [isOpen, setIsOpen] = useState(false)
 
-    const handleClick = () => {
-        setIsOpen(!isOpen)
+    const handleClick = (key) => {
+        if (key === 'open') {
+            setIsOpen(true)
+        } else {
+            setIsOpen(false)
+        }
     }
-    if (isOpen) {
-        return <NewProjectModal />
-    }
+
     return (
-        <div className="position-fixed bottom-0 end-0 me-2 mb-2 shadow-sm">
-            <button type="button" className="bg-success btn-gradient text-light rounded-circle border-0" aria-label="Add new project" style={{ width: "56px", height: "56px" }} data-bs-toggle="modal" data-bs-target="#newProject"><FaPlus /></button>
-        </div>
+        <>
+            <NewProjectModal isOpen={isOpen} setIsOpen={handleClick} />
+            <div className="rounded-circle border-0 position-fixed bottom-0 end-0 me-2 mb-2 shadow-sm">
+                <button onClick={() => handleClick('open')} type="button" className="bg-success btn-gradient text-light rounded-circle border-0" aria-label="Add new project" style={{ width: "56px", height: "56px" }}><FaPlus /></button>
+            </div>
+        </>
     )
 }
