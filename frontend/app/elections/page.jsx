e6b3b52742cbe55b95c3ef1686ab3e3d17267dc3
Mahama Hamida <mahamahamida@gmail.com>
2024-02-08 00:51:58 +0000

    Update Dashboard component with new layout and ElectionCard

--- frontend/app/elections/page.jsx
+++ frontend/app/elections/page.jsx
@@ -3,16 +3,20 @@
 import { useState, useEffect } from "react"
 import NewUserDashBoard from "@/app/components/NewUserDashBoard"
 import Button from "../components/Button"
+import ElectionCard from "../components/ElectionCard"
 
 
 export default function DashBoard() {
-    const [isNew, setIsNew] = useState(true)
+    const [isNew, setIsNew] = useState(false)
     if (isNew) {
         return <NewUserDashBoard toggle={setIsNew} isNew={isNew} />
     }
     return (
-    <div className="container">
-         <Button text={"New Project"} classNames="w-100" />
+    <div className="container d-flex flex-column gap-2 mt-4">
+        <h1 className="card-title">My Elections</h1>
+        <ElectionCard election={{title: "Election 1", description: "This is the first election", status: "In progress"}} />
+        <ElectionCard election={{title: "Election 1", description: "This is the first election", status: "Completed"}} />
+        <ElectionCard election={{title: "Election 1", description: "This is the first election", status: "Not started"}} />
     </div>
     )
 }
