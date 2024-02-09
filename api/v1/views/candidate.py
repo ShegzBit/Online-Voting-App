19fa9d09336187150c18bb5865f8dbc536d53332
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-02-10 00:44:07 +0100

    fix: re-implement api post request for list of candidates

--- api/v1/views/candidate.py
+++ api/v1/views/candidate.py
@@ -13,24 +13,34 @@ def include_candidate():
     """ Add a new candidate
     """
     data = request.get_json()
-    if not data:
-        abort(400, 'Not a JSON')
-    if not 'first_name' in data:
-        abort(400, "Missing candidate's firstname")
-    if not 'last_name' in data:
-        abort(400, "Missing candidate's lastname")
-    if not 'election_id' in data:
-        abort(400, 'Missing election_id')
-    if not 'position' in data:
-        abort(400, "Missing candidate's position of interest")
-    election_id = data['election_id']
-    election = storage.get('Election', election_id)
-    if not election:
-        abort(400, 'Election not found')
-    try:
-        candidate = Candidate(**data)
-        candidate.save()
-    except ValueError as e:
-        abort(400, str(e))
-    resp_dict = {'status': 'successful', 'candidate': candidate.to_dict()}
+    if 'candidates' not in data:
+        abort(400, 'Missing candidate')
+    if not isinstance(data['candidates'], list):
+        abort(400, 'Invalid candidate list')
+    candidates = data['candidates']
+    candidates_list = []
+    for candidate in candidates:
+        if not isinstance(candidate, dict):
+            abort(400, 'Invalid candidate')
+        if not 'first_name' in candidate:
+            abort(400, "Missing candidate's firstname")
+        if not 'last_name' in candidate:
+            abort(400, "Missing candidate's lastname")
+        if not 'election_id' in candidate:
+            abort(400, 'Missing election_id')
+        if not 'position' in candidate:
+            abort(400, "Missing candidate's position of interest")
+        election_id = candidate['election_id']
+        election = storage.get('Election', election_id)
+        if not election:
+            abort(400, 'Election not found')
+    for candidate in candidates:
+        try:
+            new_candidate = Candidate(**candidate)
+            new_candidate.save()
+            candidates_list.append(new_candidate)
+        except ValueError as e:
+            abort(400, str(e))
+    resp_dict = {'status': 'successful',
+                 'candidates': [c.to_dict() for c in candidates_list]}
     return jsonify(resp_dict), 201
