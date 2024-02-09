7da348ae0c535fedddfd32f14eba18895786a775
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-02-09 01:59:57 +0100

    Remove the .api directory

--- .api/v1/app.py
+++
@@ -1,8 +0,0 @@
-from views import app_views
-from flask import Flask, jsonify, make_response, request
-
-app = Flask(__name__)
-app.register_blueprint(app_views)
-
-if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000, debug=True)
