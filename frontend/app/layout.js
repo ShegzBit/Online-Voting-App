df4aad486ed3ad4a99e53a68382b2ba09d74bf50
Mahama Hamida <mahamahamida@gmail.com>
2024-02-10 15:43:35 +0000

    Update body class in layout.js

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -13,8 +13,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="{inter.className}">
-        <Navbar />  
+      <body className="">
+        <Navbar />
         {children}
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous" async></script>
       </body>
