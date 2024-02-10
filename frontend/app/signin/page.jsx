58016606d8da95a6452bf7b6057cbb97ea830b3a
Mahama Hamida <mahamahamida@gmail.com>
2024-02-10 15:45:55 +0000

    Refactor sign-in page layout and add forgot password link

--- frontend/app/signin/page.jsx
+++ frontend/app/signin/page.jsx
@@ -1,22 +1,29 @@
 import Button from "../components/Button"
 export default function SignInPage() {
-    return(
+    return (
         <form>
-            <div className="card " style={{borderRadius: "20px", overFlow: "hidden"}}>
-                <h1 className="card-title mb-4">Sign in</h1>
-                <p className="card-subtitle">Please enter your details to sign in</p>
-                <div className="form-floating mb-3">
-                    <input type="email" className="form-control rounded-4" id="floatingInput" placeholder="" />
-                    <label htmlFor="floatingInput">Email address</label>
-                </div>
-
-                <div className="form-floating">
-                    <input type="password" className="form-control rounded-4" id="floatingPassword" placeholder="" />
-                    <label htmlFor="floatingPassword">Password</label>
-                </div>
-                <div className="d-grid gap-2">
-                    {/* <button href="#" className="btn btn-primary">Submit</button> */}
-                    <Button text={"Sign In"} />
+            <div className="container " style={{ borderRadius: "20px", overFlow: "hidden" }}>
+                <div className="row justify-content-center">
+                    <div className='col-lg-6 col-md-6 col-sm-12'>
+                        <h1 className="card-title mb-0">Sign in</h1>
+                        <p className="card-subtitle">Please enter your details to sign in</p>
+                        <div className="form-floating mb-2">
+                            <input type="email" className="form-control rounded-4" id="floatingInput" placeholder="" />
+                            <label htmlFor="floatingInput">Email address</label>
+                        </div>
+                        <div className="form-floating">
+                            <input type="text" className="form-control rounded-4 mb-2" id="floatingPassword" placeholder="" />
+                            <label htmlFor="floatingPassword">Password</label>
+                        </div>
+                        <div className="mb-5">
+                            <a href="#" className="link-success link-offset-2 link-underline link-underline-opacity-0 " style={{ fontSize: ".8rem" }}>Forgot password?</a>
+                        </div>
+                        <div className="d-grid gap-2">
+                            {/* <button href="#" className="btn btn-primary">Submit</button> */}
+                            <Button text={"Sign In"} />
+                            <p className="mt-3 text-center">Don’t have an account? <a href="#" className=" fw-semibold link-success link-offset-2 link-underline link-underline-opacity-0">Sign Up</a></p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </form>
