4fff2d7a7cfa6987671a3bf2b9dd21e8865d35ca
Mahama Hamida <mahamahamida@gmail.com>
2024-02-12 00:40:28 +0000

    Add ElectionCreatedSucess modal component

---
+++ frontend/app/components/modals/ElectionCreatedSucess.jsx
@@ -0,0 +1,34 @@
+import Button from "../Button"
+import { useState } from "react"
+import { useRouter } from "next/navigation"
+import { Modal } from "react-bootstrap"
+
+export default function ProjectCreated({ show, onHide, electionId }) {
+    const [isOpen, setIsOpen] = useState(show)
+    const router = useRouter()
+    const handleClick = () => {
+        // onHide()
+        return router.push(`/elections/${electionId}`)
+    }
+    return (
+        <>
+            <Modal show={show}>
+                {/* <div className="modal-dialog">
+                    <div className="modal-content"> */}
+                    {/* <Modal.Header> */}
+                        {/* <Modal.Title>New Project</Modal.Title> */}
+                    {/* </Modal.Header> */}
+                        <div className="modal-body">
+                            <h1 className="mb-4" style={{ fontSize: "1.5rem" }}>Hooray! 🎉 Your project was created</h1>
+                            <p>Take it to the next level by adding additional voting options. Engage your audience and make your project shine!</p>
+                        </div>
+                        <div className="modal-footer">
+                            {/* <button onClick={handleClick} type="button" className="btn btn-outline-secondary px-5">Finish setting up</button> */}
+                            <Button cb={handleClick} text="Finish setting up" classNames="px-5 w-100" />
+                        </div>
+                    {/* </div>
+                </div> */}
+            </Modal>
+        </>
+    )
+}
