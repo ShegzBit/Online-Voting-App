bdef81595554a51bce19963831d167d463083951
Mahama Hamida <mahamahamida@gmail.com>
2024-02-12 00:43:32 +0000

    Add formatDate function to general.js

---
+++ frontend/lib/general.js
@@ -0,0 +1,16 @@
+function formatDate(dateType, date=new Date()) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hour = String(date.getHours()).padStart(2, '0')
+    const min = String(date.getMinutes()).padStart(2, '0')
+    const secs = String(date.getSeconds()).padStart(2, '0')
+
+    if (dateType === 'tag') {
+      return `${year}-${month}-${day}`
+    }
+    return `${year}-${month}-${day} ${hour}:${min}:${secs}`;
+  }
+
+
+  export { formatDate }
