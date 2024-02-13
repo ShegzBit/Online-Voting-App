13fe9408b75391c3b4c358eefbbba76adc5aa4e8
Fawaz Abdganiyu <117312471+Fawazabdganiyu@users.noreply.github.com>
2024-02-13 23:13:05 +0100

    Delete setup_mysql_test.sql

--- setup_mysql_test.sql
+++
@@ -1,8 +0,0 @@
-DROP DATABASE IF EXISTS ovs_test_db;
-
--- Prepares the MySQL server for this project
-CREATE DATABASE IF NOT EXISTS ovs_test_db;
-CREATE USER IF NOT EXISTS 'ovs_test'@'localhost' IDENTIFIED BY 'ovs_test_pwd';
-GRANT ALL PRIVILEGES ON `ovs_test_db`.* TO 'ovs_test'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'ovs_test'@'localhost';
-FLUSH PRIVILEGES;
