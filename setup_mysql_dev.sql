5b8831ffa9c9046969f7786f65c0b112efe4cc84
Fawaz Abdganiyu <117312471+Fawazabdganiyu@users.noreply.github.com>
2024-02-13 23:13:26 +0100

    Delete setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++
@@ -1,8 +0,0 @@
-DROP DATABASE IF EXISTS ovs_dev_db;
-
--- Prepares the MySQL server for this project
-CREATE DATABASE IF NOT EXISTS ovs_dev_db;
-CREATE USER IF NOT EXISTS 'ovs_dev'@'localhost' IDENTIFIED BY 'ovs_dev_pwd';
-GRANT ALL PRIVILEGES ON `ovs_dev_db`.* TO 'ovs_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'ovs_dev'@'localhost';
-FLUSH PRIVILEGES;
