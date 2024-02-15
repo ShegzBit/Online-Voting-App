5a7f5a768729d50128bd4fa90f77f6017c5171cd
Mahama Hamida <mahamahamida@gmail.com>
2024-02-15 23:50:15 +0000

    update: edit button on election card works

--- frontend/app/components/ElectionCard.jsx
+++ frontend/app/components/ElectionCard.jsx
@@ -24,7 +24,7 @@ export default function ElectionCard({ election }) {
         <div className="container p-3 rounded-2" style={{ backgroundColor: "#FAFBFC", border: "1px #E7EAEB solid" }}>
             <div className="m-0 d-flex justify-content-between align-items-center align-items-sm-center">
                 <p className="text-muted m-0" style={{ fontSize: ".8rem" }}>Online Voting</p>
-                <button className="btn p-0 m-0"><Link href={`/elections/${election.id}`}><FaRegEdit /></Link></button>
+                <Link href={`/elections/${election.id}`}><FaRegEdit /></Link>
             </div>
             <h3 className="mb-5">{election.title}</h3>
             <div className="d-flex justify-content-between align-items-center">
