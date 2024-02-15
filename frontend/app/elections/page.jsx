8b3532acdd49da2eeff5348e092c38751596ad8d
Mahama Hamida <mahamahamida@gmail.com>
2024-02-15 23:54:16 +0000

    Refactor ElectionsPage component to use user and election contexts

--- frontend/app/elections/page.jsx
+++ frontend/app/elections/page.jsx
@@ -2,33 +2,43 @@
 
 import { useState, useEffect } from "react"
 import DashBoard from "@/app/components/DashBoard"
-import { getUser } from '@/lib/authHelper';
 import { getElections } from "@/lib/electionHelper";
+import { useUser } from "../contexts/userContext";
+import { useElection } from "../contexts/electionContext"
+import { getUser } from "@/lib/authHelper";
 
 
 export default function ElectionsPage() {
     const [isEmpty, setIsEmpty] = useState(false)
     const [elections, setElections] = useState([])
+    const { user, updateUser } = useUser()
+    const { election, updateElections } = useElection()
+
+
+    if (!user) {
+        updateUser(getUser())
+    }
 
     useEffect(() => {
         const fetchElections = async () => {
             try {
-                const res = await getElections(getUser().id)
-                setElections(res)
-
-                if (res.length === 0) {
-                    setIsEmpty(true)
+                if (user?.id) {
+                    const res = await getElections(user?.id)
+                    setElections(res)
+                    updateElections(res)
+                    if (res.length === 0) {
+                        setIsEmpty(true)
+                    }
                 }
-
             } catch (e) {
                 console.log(e)
             }
         }
-
+        
         fetchElections()
-    }, [])
-
-    console.log('user', getUser().id)
+    }, [user])
+    
+    console.log(elections)
 
     return (
         <>
