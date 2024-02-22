ac88374b0ee1fac0020240879405581fc6715fd0
Mahama Hamida <mahamahamida@gmail.com>
2024-02-22 22:18:59 +0000

    Update Navbar logo link

--- frontend/app/components/core/Navbar.jsx
+++ frontend/app/components/core/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg border-bottom border-body bg-light pt-3 mb-2">
             <div className="container-fluid">
-                <a className="navbar-brand navbar-nav" href="#">
+                <a className="navbar-brand navbar-nav" href="https://pollmaster.webflow.io/">
                     <Image id="logo" src={Logo} alt="PollMaster" />
                 </a>
             </div>
