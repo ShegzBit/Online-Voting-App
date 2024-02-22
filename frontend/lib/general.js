b9453a82fd02429fecb5bb9a62bf5932dc1a4e0f
Mahama Hamida <mahamahamida@gmail.com>
2024-02-22 22:20:42 +0000

    Refactor date formatting functions and add new function for converting milliseconds into a legible string

--- frontend/lib/general.js
+++ frontend/lib/general.js
@@ -1,16 +1,30 @@
 function formatDate(dateType, date=new Date()) {
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-    const hour = String(date.getHours()).padStart(2, '0')
-    const min = String(date.getMinutes()).padStart(2, '0')
-    const secs = String(date.getSeconds()).padStart(2, '0')
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const hour = String(date.getHours()).padStart(2, '0')
+  const min = String(date.getMinutes()).padStart(2, '0')
+  const secs = String(date.getSeconds()).padStart(2, '0')
 
-    if (dateType === 'tag') {
-      return `${year}-${month}-${day}`
-    }
-    return `${year}-${month}-${day} ${hour}:${min}:${secs}`;
+  if (dateType === 'tag') {
+    return `${year}-${month}-${day}`
   }
+  return `${year}-${month}-${day} ${hour}:${min}:${secs}`;
+}
+
+function convertMilliSecondsIntoLegibleString(milliSecondsIn) {
+
+  let secsIn = milliSecondsIn / 1000;
+  let milliSecs = milliSecondsIn % 1000;
+
+  let hours = secsIn / 3600,
+  remainder = secsIn % 3600,
+  minutes = remainder / 60,
+  seconds = remainder % 60;
 
 
-  export { formatDate }
+  return ( hours.toFixed() + ": "
+          +  minutes.toFixed() + ": "
+          + seconds.toFixed() +"");
+}
+export { formatDate, convertMilliSecondsIntoLegibleString }
