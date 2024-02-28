8938b862019f4e0debe0eb6b588a01b779ed7622
Mahama Hamida <mahamahamida@gmail.com>
2024-02-28 21:38:01 +0000

    Refactor DashBoard component

--- frontend/app/components/DashBoard.jsx
+++ frontend/app/components/DashBoard.jsx
@@ -1,36 +1,39 @@
-'use client'
+"use client";
 
-import Fab from './Fab';
-import NewElection from './modals/NewElection';
-import EmptyElections from './EmptyElections';
-import ElectionCard from './ElectionCard'
-import { useElections } from '../contexts/electionsContext';
+import Fab from "./Fab";
+import NewElection from "./modals/NewElection";
+import EmptyElections from "./EmptyElections";
+import ElectionCard from "./ElectionCard";
+import { useElections } from "../contexts/electionsContext";
 
-export default function DashBoard({isEmpty }) {
-
-    return (
-        <>
-            {isEmpty ? 
-            <div className='container'>
-                <EmptyElections />
-                <NewElection />
-            </div> : <Elections />}
-            <Fab />
-        </>
-    )
+export default function DashBoard({ isEmpty }) {
+  return (
+    <>
+      {isEmpty ? (
+        <div className="container">
+          <EmptyElections />
+          <NewElection />
+        </div>
+      ) : (
+        <Elections />
+      )}
+      <Fab />
+    </>
+  );
 }
-
 
 const Elections = () => {
-    const { elections } = useElections()
-    return (
-        <div>
-        {elections && <div className="container d-flex flex-column gap-2 mt-4">
-            <h1 className="card-title">My Elections</h1>
-            {elections.map((election) => (
-                <ElectionCard key={election.id} election={election} /> 
-            ))}
-        </div>}
+  const { elections } = useElections();
+  return (
+    <div>
+      {elections && (
+        <div className="container d-flex flex-column gap-2 mt-4">
+          <h1 className="container card-title">My Elections</h1>
+          {elections.map((election) => (
+            <ElectionCard key={election.id} election={election} />
+          ))}
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
