ab7da13a14fe801179e4b012fe852813b664ed4f
Mahama Hamida <mahamahamida@gmail.com>
2024-03-04 00:28:52 +0000

    Refactor Dashboard component layout

--- frontend/app/components/DashBoard.jsx
+++ frontend/app/components/DashBoard.jsx
@@ -26,14 +26,17 @@ const Elections = () => {
   const { elections } = useElections();
   return (
     <div>
+      <div className="container">
+
       {elections && (
-        <div className="container d-flex flex-column gap-2 mt-4">
-          <h1 className="container card-title">My Elections</h1>
+        <div className=" d-flex flex-column gap-2 mt-4">
+          <h1 className="card-title">My Elections</h1>
           {elections.map((election) => (
             <ElectionCard key={election.id} election={election} />
           ))}
         </div>
       )}
+      </div>
     </div>
   );
 };
