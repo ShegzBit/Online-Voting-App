c2f881384ebf7a8aad8fa62caee6dfaaf7ae2e73
Mahama Hamida <mahamahamida@gmail.com>
2024-03-04 00:28:35 +0000

    Update Ballot component UI

--- frontend/app/components/Ballot.jsx
+++ frontend/app/components/Ballot.jsx
@@ -19,8 +19,8 @@ export default function Ballot({ electionId }) {
   return (
     <div>
       {empty && (
-        <p className="text-center text-muted mt-3">
-          You have no candidates yet. Create your first candidate now!
+        <p className="text-center text-muted mt-3 card-subtitle">
+          You have no candidates yet.
         </p>
       )}
       <hr />
@@ -37,26 +37,26 @@ function AddBallot({ electionId }) {
   };
   return (
     <div className="mt-3">
-      <div className="container">
+      {/* <div className="container"> */}
         <div className="row justify-content-center">
           <div className="col-lg-6 col-md-6 col-sm-12">
             <div
               className="d-flex px-1 rounded-1 justify-content-between align-items-center"
               style={{ backgroundColor: "#E9F2F2" }}
             >
-              <p className="m-0">Add new candidate</p>
+              <p className="m-0 p-2">Add new candidate</p>
               <button
                 onClick={() => setShow(true)}
                 type="button"
-                className="btn p-0 text-dark text-light m-0"
-                aria-label="Add new project"
+                className="btn py-0 text-dark text-light m-0"
+                aria-label="Add new ballot"
               >
                 <FaPlus />
               </button>
             </div>
           </div>
         </div>
-      </div>
+      {/* </div> */}
       <AddNewBallot electionId={electionId} show={show} onHide={handleClick} />
     </div>
   );
